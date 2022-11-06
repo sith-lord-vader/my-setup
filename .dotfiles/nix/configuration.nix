30e9bbc8330a683b3a001453ba09a243f38a4508
Abhishek Adhikari <abhiayush23@gmail.com>
2022-11-06 16:39:54 +0530

    change dir struct

--- .dotfiles/nix/configuration.nix
+++ .dotfiles/nix/configuration.nix
@@ -132,9 +132,11 @@
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
 
-  # services.fprintd.enable = true;
-  # security.pam.services.login.fprintAuth = true;
-  # security.pam.services.xscreensaver.fprintAuth = true;
+  services.fprintd.enable = true;
+  security.pam.services.login.fprintAuth = true;
+  security.pam.services.xscreensaver.fprintAuth = true;
+
+  services.gnome3.gnome-keyring.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
