2d1d5d3ea40d2e153ceb7a18828fdff972eb222a
Abhishek Adhikari <abhiayush23@gmail.com>
2022-11-09 00:28:00 +0530

    refactored nix config

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -1,18 +1,16 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [
       /etc/nixos/hardware-configuration.nix
     ];
  
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nixpkgs.config.allowUnfree = true;
+  virtualisation.docker.enable = true;
 
-  # Bootloader.
+  #*----Bootloader----
   # boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
@@ -23,42 +21,34 @@
     useOSProber = true;
     efiSupport = true;
   };
+  #!----Bootloader----
 
-  
-
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
+  #*----Networking----
+  networking.hostName = "nixos";
+  # networking.wireless.enable = true;
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
   networking.networkmanager.enable = true;
+  #!----Networking----
 
-  # Set your time zone.
+  #*----General Settings----
   time.timeZone = "Asia/Kolkata";
-
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_IN.utf8";
+  services.printing.enable = true;
+  #!----General Settings----
 
-  # Enable the X11 windowing system.
+  #*----Desktop Settings----
   services.xserver.enable = true;
-
-  # Enable the KDE Plasma Desktop Environment.
   services.xserver.displayManager.sddm.enable = true;
   services.xserver.desktopManager.plasma5.enable = true;
-
-  # Configure keymap in X11
   services.xserver = {
     layout = "us";
     xkbVariant = "";
   };
+  # services.xserver.libinput.enable = true;
+  #!----Desktop Settings----  
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
+  #*----Sound Settings----
   services.blueman.enable = true;
   sound.enable = true;
   hardware.pulseaudio.enable = false;
@@ -69,18 +59,13 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
     #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
     #media-session.enable = true;
   };
+  #!----Sound Settings----
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+  #*----User Account----
   users.users.xpert = {
     isNormalUser = true;
     description = "Abhishek Adhikari";
@@ -88,21 +73,18 @@
     packages = with pkgs; [
       firefox
       kate
-    #  thunderbird
     ];
   };
+  #!----User Account----
 
+  #*----Fonts----
   fonts.fonts = with pkgs; [
     (nerdfonts.override { fonts = [ "FiraCode" "Hack" ]; })
   ];
+  #!----Fonts----
 
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
+  #*----System Packages----
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     git
     google-chrome
@@ -119,46 +101,34 @@
     discord
     docker
     anydesk
+    openssl
   ];
+  #!----System Packages----
 
+  #*----OpenVPN Setup----
   environment.etc.openvpn.source = "${pkgs.update-resolv-conf}/libexec/openvpn";
+  #!----OpenVPN Setup
 
+  #*----SSH Setup----
   programs.ssh.startAgent = true;
- 
-  programs.zsh.enable = true;
-  users.defaultUserShell = pkgs.zsh;
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
+  #!----SSH Setup
 
+  programs.zsh.enable = true;
+  users.defaultUserShell = pkgs.zsh;  
+
+  #*----User Access Settings----
   # services.fprintd.enable = true;
   # security.pam.services.login.fprintAuth = true;
   security.sudo.wheelNeedsPassword = false;
   services.gnome.gnome-keyring.enable = true;
+  #!----User Access Settings----
 
-  virtualisation.docker.enable = true;
-
-  # Open ports in the firewall.
+  #*----Firewall----
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  networking.firewall.enable = false;
+  #!----Firewall----
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "22.05"; # Did you read the comment?
-
+  system.stateVersion = "22.05"; 
 }
--- shell/zsh.sh
+++ shell/zsh.sh
@@ -1,3 +1,5 @@
+[ -z "$SSH_AUTH_SOCK" ] && eval "$(ssh-agent -s)"
+
 sudo () {
   local command=$@
   read -r "REPLY?Authorize command $(echo \"$@\") be  executed? (y/N): "
