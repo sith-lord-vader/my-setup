21eb56fbd8154a66f5053b49499e8b65faa07f80
Abhishek Adhikari <abhiayush23@gmail.com>
2022-12-17 01:27:00 +0530

    removed extra inputs from flake

--- nix/flake.nix
+++ nix/flake.nix
@@ -1,15 +1,13 @@
 {
   inputs.nixpkgs = { url = "github:nixos/nixpkgs/nixos-22.11"; };
-  inputs.bulla = { url = "git+ssh://git@tree.mn/autoopt-hadoop-devops/remote-systemd-controller?ref=master"; };
-  inputs.gitit-wiki = { url = "path:/home/xpert/Work/gitit-config"; };
-  inputs.test = { url = "path:/home/xpert/Work/nix-pkg-module-boilerplate"; };
-  outputs = { self, nixpkgs, bulla, gitit-wiki, test }:
+  inputs.test = { url = "github:abhishekadhikari23/nix-pkg-module-boilerplate"; };
+  outputs = { self, nixpkgs, test }:
     let
     in {
       nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
-        modules = [ bulla.nixosModules.stormtrooper gitit-wiki.nixosModules.gitit-wiki test.nixosModules.module_name ./configuration.nix ];
-        specialArgs = { inherit bulla; inherit gitit-wiki; inherit test; };
+        modules = [ test.nixosModules.module_name ./configuration.nix ];
+        specialArgs = { inherit test; };
       };
     };
 }
