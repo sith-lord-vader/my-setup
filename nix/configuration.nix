2ee576e3ae7247c27e1219bd69fde8b1a7a57958
Abhishek Adhikari <abhiayush23@gmail.com>
2024-11-19 12:11:31 +0530

    major update

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -1,36 +1,32 @@
-{ config, lib, pkgs, home-manager, vscode-server, ... }:
+{ config, lib, pkgs, home-manager, vscode-server, pkgs-extras, certify-rebellion, cloudflare-warp, ... }:
 
 {
   imports = let
 
-  in
-    [
-      ./machine/mnet.nix
-      #./modules/wireguard.nix
-      home-manager.nixosModules.home-manager
-      vscode-server.nixosModules.default
-    ];
+  in [
+    ./machine/mnet.nix
+    #./modules/wireguard.nix
+    home-manager.nixosModules.home-manager
+    vscode-server.nixosModules.default
+    certify-rebellion.nixosModules.certify-rebellion
+    cloudflare-warp.nixosModules.cloudflare-warp
+  ];
 
   nix.settings = {
     experimental-features = [ "nix-command" "flakes" "repl-flake" ];
     auto-optimise-store = true;
   };
-  
+
   nix.gc = {
     automatic = true;
     dates = "weekly";
-    options = "--delete-older-than 7d";
-  };
-  
-  nixpkgs.config = {
-    # Allow unfree packages
-    allowUnfree = true;
+    options = "--delete-older-than 60d";
   };
 
-  nixpkgs.config.permittedInsecurePackages = [
-    "electron-25.9.0"
-  ];
-  
+  nixpkgs.config.allowUnfree = true;
+
+  nixpkgs.config.permittedInsecurePackages = [ "electron" "electron-27.3.11" ];
+
   virtualisation.docker.enable = true;
   virtualisation.lxd.enable = true;
   services.flatpak.enable = true;
@@ -41,6 +37,11 @@
     enable = true;
     configurationLimit = 5;
   };
+  boot.kernelModules = [
+    "evdi"
+  ];
+
+  programs.cloudflare-warp.enable = true;
   #boot.loader.efi.canTouchEfiVariables = true;
   #boot.loader.efi.efiSysMountPoint = "/boot/efi";
   #boot.loader.grub = {
@@ -77,13 +78,25 @@
 
   #*----Desktop Settings----
   services.xserver.enable = true;
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
   services.xserver.desktopManager.xfce.enable = true;
+  services.xserver.videoDrivers = [ "displaylink" "modesetting" ];
+#   services.xserver.config = ''
+# Section "ServerLayout"
+#   Identifier   "Main"
+#   Screen       0 "Primary"
+#   Screen       1 "DellPortraitLeft" RightOf "Primary"
+#   Screen       2 "Wacom" RightOf "DellPortraitLeft"
+#   Screen       3 "U2412" LeftOf "Primary"
+#   Option         "Xinerama" "1"  # enable XINERAMA extension.  Default is disabled.
+# EndSection  
+#   '';
   services.xserver = {
-    layout = "us";
-    xkbVariant = "";
+    xkb.layout = "us";
+    xkb.variant = "";
   };
+  # services.hardware.bolt.enable = true;
   # services.xserver.libinput.enable = true;
   #!----Desktop Settings----  
 
@@ -110,14 +123,20 @@
     isNormalUser = true;
     description = "Abhishek Adhikari";
     extraGroups = [ "networkmanager" "wheel" "docker" "lxd" "rslsync" "nginx" ];
-    packages = with pkgs; [
-    ];
+    packages = with pkgs; [ ];
   };
+
+  # programs.weylus = {
+  #   enable = true;
+  #   users = [ "xpert" ];
+  #   openFirewall = true;
+  # };
 
   home-manager.users.xpert = {
     home.stateVersion = "23.05";
     home.username = "xpert";
     home.homeDirectory = "/home/xpert";
+    home.enableNixpkgsReleaseCheck = false;
     programs.home-manager.enable = true;
 
     programs = {
@@ -153,9 +172,9 @@
           export PATH=$HOME/.my-setup/bin:$PATH
           alias ssh="ssh -A"
 
-          source $HOME/.bitwarden-keys.sh
-          setup_bitwarden
-          export BW_SESSION=$(bw unlock --passwordenv BW_MASTER --raw)
+          # source $HOME/.bitwarden-keys.sh
+          # setup_bitwarden
+          # export BW_SESSION=$(bw unlock --passwordenv BW_MASTER --raw)
         '';
         shellAliases = {
           ne = "nix-env";
@@ -171,7 +190,10 @@
             { name = "zsh-users/zsh-autosuggestions"; }
             { name = "zsh-users/zsh-syntax-highlighting"; }
             { name = "zdharma-continuum/fast-syntax-highlighting"; }
-            { name = "marlonrichert/zsh-autocomplete"; tags = [ depth:1 ]; }
+            {
+              name = "marlonrichert/zsh-autocomplete";
+              tags = [ "depth:1" ];
+            }
           ];
         };
         oh-my-zsh = {
@@ -186,9 +208,8 @@
   #!----User Account----
 
   #*----Fonts----
-  fonts.packages = with pkgs; [
-    (nerdfonts.override { fonts = [ "FiraCode" "Hack" "Meslo" ]; })
-  ];
+  fonts.packages = with pkgs;
+    [ (nerdfonts.override { fonts = [ "FiraCode" "Hack" "Meslo" ]; }) ];
   #!----Fonts----
 
   #*----System Packages----
@@ -196,6 +217,7 @@
     #*---work---
     exfat
     wget
+    plasma5Packages.plasma-thunderbolt
     expect
     git
     google-chrome
@@ -215,6 +237,11 @@
     nodejs_20
     gnupg
     tmux-cssh
+    nixpkgs-fmt
+    outils
+    pkgs-extras.elasticsearch8
+    tmux
+    nixfmt-rfc-style
     # netmaker.packages.x86_64-linux.netmaker
     #!---work---
 
@@ -236,6 +263,10 @@
     spotify-tray
     pinentry
     libsForQt5.kcalc
+    zoom-us
+    zenith
+    figma-linux
+    figma-agent
     #!---misc---
 
     #*---test pkgs---
@@ -267,7 +298,7 @@
   services.pcscd.enable = true;
   programs.gnupg.agent = {
     enable = true;
-    pinentryFlavor = "curses";
+    pinentryPackage = pkgs.pinentry-curses;
   };
 
   #*----Firewall----
@@ -277,38 +308,124 @@
   #!----Firewall----
 
   #*---settings node-exporter---
-  services.prometheus =
-    {
-      exporters = {
-        node = {
-          enable = true;
-          enabledCollectors = [ "systemd" ];
-          port = 9002;
-        };
-      };
-    };
-  #!---settings node-exporter---
-
-  services.grafana = {
-    enable = true; 
-    settings = {
-      server = {
+  services.prometheus = {
+    exporters = {
+      node = {
+        enable = true;
+        enabledCollectors = [ "systemd" ];
+        port = 9002;
       };
     };
   };
+  #!---settings node-exporter---
 
+  services.grafana = {
+    enable = true;
+    settings = { server = { }; };
+  };
 
   programs.nix-ld.enable = true;
-  #environment.variables = {
-  #    NIX_LD_LIBRARY_PATH = lib.makeLibraryPath [
-  #      pkgs.stdenv.cc.cc
-  #    ];
-  #    #NIX_LD = lib.fileContents "${pkgs.stdenv.cc}/nix-support/dynamic-linker";
-  #};
+  environment.variables = {
+    #  NIX_LD_LIBRARY_PATH = lib.makeLibraryPath [
+    #    pkgs.stdenv.cc.cc
+    #  ];
+    #  #NIX_LD = lib.fileContents "${pkgs.stdenv.cc}/nix-support/dynamic-linker";
+    PLASMA_USE_QT_SCALING=1;
+    QT_SCREEN_SCALE_FACTORS="eDP-1=1.25;DVI-I-2-2=1;DVI-I-1-1=1";
+  };
 
   environment.shells = with pkgs; [ zsh ];
 
   #security.pki.certificateFiles = [ "/home/xpert/.my-setup/OpenWrt.pem" "/etc/CAPrivate.pem" ];
 
   system.stateVersion = "23.05";
+
+  # services.haproxy = {
+  #   enable = true;
+  #   config = "";
+  # };
+
+  # environment.etc.haproxy.source = "/home/xpert/haproxy.conf";
+
+  # systemd.services.haproxy.serviceConfig = {
+  #   ExecStartPre = lib.mkForce [
+  #     # when the master process receives USR2, it reloads itself using exec(argv[0]),
+  #     # so we create a symlink there and update it before reloading
+  #     "${pkgs.coreutils}/bin/ln -sf ${
+  #       lib.getExe pkgs.haproxy
+  #     } /run/haproxy/haproxy"
+  #     # when running the config test, don't be quiet so we can see what goes wrong
+  #     "/run/haproxy/haproxy -c -f /etc/haproxy"
+  #   ];
+  #   ExecStart = lib.mkForce
+  #     "/run/haproxy/haproxy -Ws -f /etc/haproxy -p /run/haproxy/haproxy.pid";
+  #   ExecReload = lib.mkForce [
+  #     "${lib.getExe pkgs.haproxy} -c -f /etc/haproxy"
+  #     "${pkgs.coreutils}/bin/ln -sf ${
+  #       lib.getExe pkgs.haproxy
+  #     } /run/haproxy/haproxy"
+  #     "${pkgs.coreutils}/bin/kill -USR2 $MAINPID"
+  #   ];
+  #   ProtectHome = lib.mkForce "read-only";
+  # };
+
+  services.elasticsearch = {
+    enable = true;
+    listenAddress = "0.0.0.0";
+    single_node = false;
+    package = pkgs-extras.elasticsearch8;
+    cluster_name = "es";
+    # restartIfChanged = false;
+    plugins = [ ];
+
+    extraConf = ''
+      discovery.seed_hosts: ["127.0.0.1"]
+      node.name: es-1
+      node.roles: [ master, data ]
+      node.processors: 2
+      cluster.initial_master_nodes: ["es-1"]
+      http.cors.enabled: true
+      http.cors.allow-origin: '*'
+      thread_pool.search.queue_size: 600
+      xpack.security.enabled: false
+      xpack.security.transport.ssl.enabled: false
+      xpack.security.http.ssl.enabled: false
+      reindex.remote.whitelist: "apps.dakh.tech:80"
+    '';
+
+    extraJavaOptions = [
+      "-Xmx4G"
+      "-Xms4G"
+
+      # "-XX:-UseConcMarkSweepGC"
+      # "-XX:-UseCMSInitiatingOccupancyOnly"
+      "-XX:+UseG1GC"
+      "-XX:InitiatingHeapOccupancyPercent=75"
+
+      "-Des.networkaddress.cache.ttl=60"
+      "-Des.networkaddress.cache.negative.ttl=10"
+
+      "-XX:-HeapDumpOnOutOfMemoryError"
+      "-XX:HeapDumpPath=/dev/null"
+
+      "-XX:ErrorFile=/var/log/elasticsearch/hs_err_pid%p.log"
+
+      "-XX:+PreserveFramePointer"
+
+      # "-Xlog:gc*,gc+age=trace,safepoint:file=/var/log/elasticsearch/logs/gc.log:utctime,pid,tags:filecount=32,filesize=64m"
+    ];
+  };
+
+  services.jupyterhub = {
+    enable = true;
+    extraConfig = ''
+c.Application.log_level = 'DEBUG'
+c.Authenticator.allow_all = True
+    '';
+  };
+
+  # services.certify-rebellion = {
+  #   enable = false;
+  #   domains = [ "*.srv.media.net" "*.media.net" "*.internal.reports.mn" ];
+  # };
 }
--- nix/flake.nix
+++ nix/flake.nix
@@ -1,18 +1,26 @@
 {
   inputs = {
     nixpkgs = { url = "github:nixos/nixpkgs/nixos-unstable"; };
+    nixpkgs-mirror = { url = "git+file:///home/xpert/work/nixpkgs-mirror?ref=es8"; };
+    cloudflare-warp = { url = "git+file:///home/xpert/work/cloudflare-warp-nix"; };
     vscode-server.url = "github:nix-community/nixos-vscode-server";
     home-manager = { url = "github:nix-community/home-manager"; };
+    certify-rebellion = { url = "git+file:///home/xpert/work/certify-rebellion"; };
   };
-  outputs = { self, nixpkgs, vscode-server, home-manager, ... }:
+  outputs = { self, nixpkgs, vscode-server, home-manager, nixpkgs-mirror, certify-rebellion, cloudflare-warp, ... }:
     let
+      pkgs-extras = import nixpkgs-mirror {
+        system = "x86_64-linux";
+        config.allowUnfree = true;
+        config.permittedInsecurePackages = [ "electron" "electron-27.3.11" ];
+      }; 
     in {
       nixosConfigurations.death-star = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
-        modules = [ 
+        modules = [
           ./configuration.nix
         ];
-        specialArgs = { inherit home-manager; inherit vscode-server; };
+        specialArgs = { inherit home-manager; inherit vscode-server; inherit pkgs-extras; inherit certify-rebellion; inherit cloudflare-warp; };
       };
     };
 }
